b49c97bc61f37b9b7ab066166fc0ef1f6b638398
navibryle <naviivan321@gmail.com>
2023-04-08 18:25:37 -0600

    removed untracked files

--- nvim/lua/uwu/aerial.lua
+++
@@ -1,9 +0,0 @@
-require("aerial").setup {
-  on_attach = function(bufnr)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>a", "<cmd>AerialToggle!<CR>", {})
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "{", "<cmd>AerialPrev<CR>", {})
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "}", "<cmd>AerialNext<CR>", {})
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "[[", "<cmd>AerialPrevUp<CR>", {})
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "]]", "<cmd>AerialNextUp<CR>", {})
-  end,
-}
--- nvim/lua/uwu/notif.lua
+++
@@ -1,7 +0,0 @@
-
-vim.notify.setup {
-  fps = 1,
-  render = "minimal",
-  stages = "static",
-  timeout = 1000,
-}
