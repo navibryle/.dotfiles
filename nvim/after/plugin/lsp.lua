fe2736f2310a3b5a5eaf21b1f599d17f1fea412b
navibryle <naviivan321@gmail.com>
2025-08-11 10:17:37 -0700

    fix lsp config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -183,14 +183,12 @@ vim.lsp.config("cssls", {
   capabilities = capabilities2,
 })
 vim.lsp.config("eslint", {
-  {
-    on_attach = function(client, bufnr)
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        buffer = bufnr,
-        command = "EslintFixAll",
-      })
-    end,
-  },
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
 })
 vim.lsp.config("ts_ls", {
   cmd = { "typescript-language-server", "--stdio" },
