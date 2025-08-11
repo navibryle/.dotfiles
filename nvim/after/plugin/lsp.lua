9900e244514bcca469e7cb96647f071ba281038b
navibryle <naviivan321@gmail.com>
2025-08-11 10:54:32 -0700

    fix autocmd

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -112,6 +112,34 @@ vim.lsp.enable({
   "bashls",
   "cssls",
 })
+local function fix_all(opts)
+  opts = opts or {}
+
+  local bufnr = opts.bufnr or vim.api.nvim_get_current_buf()
+  vim.validate("bufnr", bufnr, "number")
+
+  local client = opts.client or vim.lsp.get_clients({ bufnr = bufnr, name = "eslint" })[1]
+
+  if not client then return end
+
+  local request
+
+  if opts.sync then
+    request = function(buf, method, params) client:request_sync(method, params, nil, buf) end
+  else
+    request = function(buf, method, params) client:request(method, params, nil, buf) end
+  end
+
+  request(bufnr, "workspace/executeCommand", {
+    command = "eslint.applyAllFixes",
+    arguments = {
+        {
+            uri = vim.uri_from_bufnr(bufnr),
+            version = vim.lsp.util.buf_versions[bufnr],
+        },
+    },
+  })
+end
 vim.lsp.config("*", {
   capabilities = capabilities,
   on_attach = defaultAttach,
@@ -189,6 +217,9 @@ vim.lsp.config("eslint", {
       command = "EslintFixAll",
     })
   end,
+  on_init = function(client)
+      vim.api.nvim_create_user_command("EslintFixAll", function() fix_all({ client = client, sync = true }) end, {})
+    end,
 })
 vim.lsp.config("ts_ls", {
   cmd = { "typescript-language-server", "--stdio" },
